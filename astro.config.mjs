e95f3cd447884868a3188a07e7e0b8e615024354
quietbuilds tech <quietbuilds-tech>
2026-02-08 14:21:43 -0500

    fixing astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://quietbuilds-tech.github.io",
   base: "/telugu-panchangam-usa",
+  trailingSlash: "always",
 });
